4dc2f3f971edfe572a1c79cb4c2eb40eb611d2d4
batoxa <nbatorin@gmail.com>
2021-08-27 01:31:52 +0300

    sketch NewsPage

---
+++ src/components/NewsPage/NewsPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NewsPage = (props) => {
+    return (
+        <div>
+            <div>News Link</div>
+            <div>News Header</div>
+            <div>News Date</div>
+            <div>News Author</div>
+            <div>News Comments Count</div>
+        </div>
+    );
+};
+
+export { NewsPage };
---
+++ src/components/NewsPage/NewsPageContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NewsPage } from "./NewsPage";
+
+const NewsPageContainer = (props) => {
+    return (
+        <div>
+            <NewsPage />
+        </div>
+    );
+};
+
+export { NewsPageContainer };
